ffad4ebb525ea8f4cc2762c6e4c5bb1f874604c6
Jungmin Ji <fdev.jm@gmail.com>
2017-05-27 21:57:34 +0900

    done for 0527

--- project/youyu.com/js/script.js
+++ project/youyu.com/js/script.js
@@ -1,5 +1,5 @@
-$( () => {
-
+$(() => {
+  // Testing code
   let body = $('body');
 
   let myMenu = {
@@ -14,23 +14,52 @@ $( () => {
   };
 
 
-  $(window).keyup( (e) => {
+  $(window).keyup((e) => {
 
-    if(e.keyCode === 81){
-      for(key in myMenu){
+    if (e.keyCode === 81) {
+      for (key in myMenu) {
         myMenu[key].toggle();
       }
-      for(key in quickMenu){
+      for (key in quickMenu) {
         quickMenu[key].toggle();
       }
     }
 
-    if(e.keyCode === 87){
+    if (e.keyCode === 87) {
       $('section').toggleClass('component-bg-color');
       $('header').toggleClass('component-bg-color')
       $('footer').toggleClass('component-bg-color')
     }
 
   })
+
+
+  // Favorite button
+  $('.btn-favorite').on('click', (e) => {
+    $(e.currentTarget).toggleClass('active');
+  })
+
+  // Rate button
+  $('.item-rate button').on('click', (e) => {
+
+    let btn = $(e.currentTarget);
+    if (btn.is(':first-child')) {
+      btn.toggleClass('rated');
+    } else {
+      if (btn.hasClass('rated')) {
+        btn.nextAll().removeClass('rated');
+        btn.addClass('rated');
+      } else {
+        btn.toggleClass('rated');
+        btn.prevAll().addClass('rated');
+      }
+    }
+
+
+    // $(e.currentTarget).toggleClass('rated');
+    //
+    // console.log($(e.currentTarget).index());
+  })
+
 
 })
