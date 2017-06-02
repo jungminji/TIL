c04a06696c893ad973a1b3ab35e96753bdca70e1
Jungmin Ji <fdev.jm@gmail.com>
2017-06-02 17:52:38 +0900

    .

--- javascript/libs/Ein.js
+++ javascript/libs/Ein.js
@@ -1,61 +1,114 @@
-(function() {
+"use strict"
 
-  // methods hold functions
-  const methods = {
+class Ein {
 
-    Math: {
-      sum: function(...num){
-        let sum = 0;
-        for(let i = 0; i < arguments.length; i++){
-          sum += arguments[i];
-        }
-        return sum;
-      },
+  constructor() {}
 
-      avg: function(...num) {
-        console.log(arguments);
-        return this.sum(...num) / arguments.length;
+  sum(...num) {
 
-      },
-
-      max: function(...num) {
-
-        let max = arguments[0];
-
-        for (let i = 0; i < arguments.length; i++) {
-          max < arguments[i] ? max = arguments[i] : false;
-        }
-
-        return max;
-      }
-    },
-    type: function(data) {
-      return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
-    },
-
-    isType: function(data, type) {
-
-      if (this.type(type) !== 'string') {
-        throw ': Invalid parameter type on argument 2\nMust present data type';
-      }
-
-      return this.type(data) === type;
-    },
-    strConcat: function(...str) {
-      let str = "";
-
-      for (let i = 0; i < arguments.length; i++) {
-        str += arguments[i];
-      }
-
-      return str;
+    if (arguments.length === 0) {
+      throw ": At least one parameter value is requied";
     }
+
+    let sum = 0;
+    for (let item of arguments) {
+      sum += item;
+    }
+    return sum;
   }
 
-  // Ein's prototype => methods
-  const Ein = Object.create(methods);
+  /**
+   * Returns max value in Parameters
+   *  @function max
+   *  @param {number} numbers
+   *  @returns {number} max number
+   */
+  max(...num) {
 
-  // Add Ein to window object
-  window.Ein = Ein;
+    if (arguments.length === 0) {
+      throw ": At least one parameter value is requied";
+    }
 
-})();
+    let max = arguments[0];
+    for (let item of arguments) {
+      max < item ? max = item : false;
+    }
+    return max;
+  }
+
+  /**
+   *  Returns random number, it can take single parameter or two parameters to set range
+   *  @function randNum
+   *  @param {number} number
+   *  @returns {number} random number
+   */
+  randNum(min, max) {
+
+    let num = 0;
+
+    if (this.type(min) === "undefined") {
+      throw ": At least one parameter value is required";
+    }
+
+    return this.type(max) === "undefined" ?
+      num = Math.floor(Math.random() * min + 1) :
+      num = Math.floor(Math.random() * ((max + 1) - min) + min);
+  }
+
+  type(data) {
+    return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
+  }
+
+  isType(data, type) {
+    if (this.type(type) !== 'string') {
+      throw ': Invalid parameter type on argument 2\nMust present data type';
+    }
+    return this.type(data) === type;
+  }
+
+  strConcat(...str) {
+    let str = "";
+    for (let item of arguments) {
+      str += item;
+    }
+    return str;
+  }
+
+  /**
+   * Returns day in Korean
+   *  @function todayKR
+   *  @returns {string} today in Korean
+   */
+  todayKR() {
+
+    let day = "";
+
+    switch ( (new Date).getDay() ) {
+      case 1:
+        day = "월요일";
+        break;
+      case 2:
+        day = "화요일";
+        break;
+      case 3:
+        day = "수요일";
+        break;
+      case 4:
+        day = "목요일";
+        break;
+      case 5:
+        day = "금요일";
+        break;
+      case 6:
+        day = "토요일";
+        break;
+      default:
+        day = "일요일";
+        break;
+    }
+    return day;
+  }
+
+}
+
+const _ = new Ein();
