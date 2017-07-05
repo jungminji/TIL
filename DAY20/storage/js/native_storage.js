b08ce9429d216d6f140221d4ca976f3766097705
jungminji <fdev.jm@gmail.com>
2017-07-05 14:28:38 +0900

    .

--- DAY20/storage/js/native_storage.js
+++ DAY20/storage/js/native_storage.js
@@ -5,10 +5,14 @@
     let document = window.document;
     let memo = document.querySelector('.memo');
     let memoBtns = document.querySelectorAll('button');
+    let memoTitle = memo.querySelector('#memo-title');
+    let memoContent = memo.querySelector('#memo-content');
+    let memoItem;
     let memoStorage;
 
     function init() {
         loadMemoData(memoDataID);
+        render();
         eventBind();
     }
 
@@ -17,12 +21,52 @@
 
         if (!loadedData) {
             window.sessionStorage.setItem(id, JSON.stringify([]));
-        } else {
-            memoStorage = JSON.parse(loadedData);
+            loadedData = window.sessionStorage.getItem(id);
         }
+        memoStorage = JSON.parse(loadedData);
+    }
+
+    function render() {
+        let memoContainer = document.querySelector('.memo-item-container');
+        let template = '';
+
+        memoContainer.innerHTML = '';
+
+        memoStorage.forEach(function (item, index) {
+            template += `
+        <article class="memo-item column is-12 message is-primary">
+            <div class="message-header">
+              <h5 class="memo-item-title">${item.title}</h5>
+              <button data-remove="${index}" type="button" class="delete" aria-label="메모 아이템 제거"></button>
+            </div>
+            <div class="message-body">
+              <p class="memo-item-content">${item.content}</p>
+            </div>
+        </article>
+            `;
+        });
+
+        memoContainer.innerHTML = template;
+
+        // Add eventlistener on buttons
+        let removeBtns = document.querySelectorAll('.delete');
+        [].forEach.call(removeBtns, function (button) {
+            button.addEventListener('click', removeMemo);
+        });
+    }
+
+    function removeMemo(e) {
+        if (e.target.localName === 'button' && e.target.classList.contains('delete')) {
+            let deleteItemID = e.target.dataset.remove;
+            memoStorage.splice(deleteItemID, 1);
+            window.sessionStorage.setItem(memoDataID, JSON.stringify(memoStorage));
+            render();
+        }
+        e.stopPropagation();
     }
 
     function eventBind() {
+
         [].forEach.call(memoBtns, function (button) {
             button.addEventListener('click', detectButton);
         });
@@ -32,12 +76,12 @@
         this.classList.contains('is-save') ? saveMemo() : cancelMemo();
     }
 
+
     function saveMemo() {
-        let memoTitle = memo.querySelector('#memo-title');
-        let memoContent = memo.querySelector('#memo-content');
-        let memoItem;
+
 
         if (validateMemo(memoTitle, memoContent)) {
+            alert('Write Title & Content');
             return;
         }
 
@@ -51,8 +95,9 @@
         // Store into local storage (5MB approx.)
         window.sessionStorage.setItem(memoDataID, JSON.stringify(memoStorage));
 
-        memoTitle.value = '';
-        memoContent.value = '';
+        cancelMemo();
+
+        render();
     }
 
     function validateMemo(title, content) {
@@ -60,7 +105,8 @@
     }
 
     function cancelMemo() {
-
+        memoTitle.value = '';
+        memoContent.value = '';
     }
 
     init();
